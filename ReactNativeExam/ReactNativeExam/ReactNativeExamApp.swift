c3973e3db9ac545c275bf4c6c41354e944227e0d
asta3090 <asta3090@stud.kea.dk>
2022-05-24 21:58:00 +0200

    "Huuuuge mistake"

---
+++ ReactNativeExam/ReactNativeExam/ReactNativeExamApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReactNativeExamApp.swift
+//  ReactNativeExam
+//
+//  Created by Asta Vittrup Graversen on 24/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct ReactNativeExamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
